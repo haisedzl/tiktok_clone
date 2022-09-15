105abdad3460665248ee1bef30c983f1c91c724c
Your Name <you@example.com>
2022-09-15 11:32:26 +0700

    loading

--- src/pages/Home/ContenVideo/HandleVideo.js
+++ src/pages/Home/ContenVideo/HandleVideo.js
@@ -1,15 +1,21 @@
 import { useEffect, useMemo, useState } from 'react';
+
+
+
 const useElementOnScreen = (options, targetRef) => {
+
     const [isVisibile, setIsVisible] = useState();
     const callbackFunction = (entries) => {
-        const [entry] = entries; //const entry = entries[0]
+    const [entry] = entries; //const entry = entries[0]
         setIsVisible(entry.isIntersecting);
     };
     const optionsMemo = useMemo(() => {
         return options;
     }, [options]);
     
+
     useEffect(() => {
+
         const observer = new IntersectionObserver(callbackFunction, optionsMemo);
         const currentTarget = targetRef.current;
         if (currentTarget) observer.observe(currentTarget);
